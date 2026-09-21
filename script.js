b1228037b5e8c8b49a8ff1822c4d02035701ce15
gis-parks-swiss <107912202+gis-parks-swiss@users.noreply.github.com>
2026-09-21 11:29:40 +0200

    Change color property to black in script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function createTrailSymbol(scale, colors) {
 						capType: "ROUND",
 						joinType: "ROUND",
 						width: 4 * scaleFactor,
-						color: colors.main,
+						color: [0,0,0],
 					},
 				],
 			},
