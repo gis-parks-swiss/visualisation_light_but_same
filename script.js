003f9e5c9fa63a0ff2fe028f9f517e8e2fee4b36
gis-parks-swiss <107912202+gis-parks-swiss@users.noreply.github.com>
2026-09-21 11:30:33 +0200

    Change color to use fade from colors object

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function createTrailSymbol(scale, colors) {
 						capType: "ROUND",
 						joinType: "ROUND",
 						width: 4 * scaleFactor,
-						color: [0,0,0],
+						color: colors.fade,
 					},
 				],
 			},
